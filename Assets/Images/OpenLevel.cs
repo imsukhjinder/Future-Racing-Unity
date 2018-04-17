778da817ff757b1a85807bd4888267c9083eab04
Sukhjinder Singh <32549592+imsukhjinder@users.noreply.github.com>
2018-04-17 21:03:29 +0530

    Initial commit

---
+++ Assets/Images/OpenLevel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLevel : MonoBehaviour {
+
+	public void NextGO(){
+		Application.LoadLevel ("Level2");
+	}
+}
---
+++ Assets/Images/OpenScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenScene : MonoBehaviour {
+
+	public void GoNext(){
+		Application.LoadLevel ("Level1");
+	}
+}
